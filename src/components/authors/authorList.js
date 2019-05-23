1f0ab7fe25526cf6aece49d9bd2f7e3ebbb0f75e
Jaime Villarreal <jvillarreal337@gmail.com>
2019-05-22 20:36:06 -0500

    Changed routing to React Router.

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -3,32 +3,34 @@
 var React = require('react');
 
 var AuthorList = React.createClass({
-    propTypes: {
-        authors: React.propTypes.array.isRequired
-    },
-    render: function() {
-        var createAuthorRow = function(author) {
-            return (
-                <tr key={author.id}>
-                    <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
-                    <td>{author.firstName} {author.lastName}</td>
-                </tr>
-            );
-        };
-        return (
-            <div>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </thead>
-                    <tbody>
-                        {this.props.authors.map(createAuthorRow, this)}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+	propTypes: {
+		authors: React.PropTypes.array.isRequired
+	},
+
+	render: function() {
+		var createAuthorRow = function(author) {
+			return (
+				<tr key={author.id}>
+					<td><a href={"/#authors/" + author.id}>{author.id}</a></td>
+					<td>{author.firstName} {author.lastName}</td>
+				</tr>
+			);
+		};
+
+		return (
+			<div>
+				<table className="table">
+					<thead>
+						<th>ID</th>
+						<th>Name</th>
+					</thead>
+					<tbody>
+						{this.props.authors.map(createAuthorRow, this)}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 });
 
 module.exports = AuthorList;
--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -5,26 +5,26 @@ var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
-    getInitialState: function() {
-        return {
-            authors: []
-        };
-    },
+	getInitialState: function() {
+		return {
+			authors: []
+		};
+	},
 
-    componentDidMount: function() {
-        if(this.isMounted()) {
-            this.setState({ authors: AuthorApi.getAllAuthors() });
-        }     
-    },
+	componentDidMount: function() {
+		if (this.isMounted()) {
+			this.setState({ authors: AuthorApi.getAllAuthors() });
+		}
+	},
 
-    render: function() {
-        return (
-            <div>
-                <h1>Authors</h1>
-                <AuthorList authors={this.state.authors} />
-            </div>
-        );
-    }
+	render: function() {
+		return (
+			<div>
+				<h1>Authors</h1>
+				<AuthorList authors={this.state.authors} />
+			</div>
+		);
+	}
 });
 
 module.exports = AuthorPage;
